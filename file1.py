c09cf858b7eb3c9c6095ac87e1e9447761722195
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-07 08:02:38 +0300

    init commit

--- file1.py
+++ file1.py
@@ -1,2 +1,13 @@
-print("file1")
-print ("file1")
+def filter_by_state(data_list, state='EXECUTED'):
+    """Сортирует список по введенному ключу"""
+    filtered_list = []
+    for data in data_list:
+        if data.get('state') == state:
+            filtered_list.append(data)
+            return filtered_list
+        else:
+            return "Некорректный ввод"
+
+
+
+
